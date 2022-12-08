b029306650d6c88629dbbc5ec3d91a10e33ddc30
faizanshahid232 <75093651+faizanshahid232@users.noreply.github.com>
2022-12-08 18:23:08 +0400

    second changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import React from 'react';
 import { Routes , Route } from 'react-router-dom';
 import SuccessFailure from './components/SuccessFail';
+import Fail from './components/Fail';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route exact path='/' element={<Header/>}></Route>
       <Route exact path='/success' element={<SuccessFailure/>}></Route>
+      <Route exact path='/invalidcode' element={<Fail/>}></Route>
     </Routes>
     </>
   );
--- src/components/SuccessFail.jsx
+++ src/components/SuccessFail.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
 
 const SuccessFailure = () => {
-    let success_failure = true;
     return (
         <>
         <div className='bg-[#181b1d] relative flex flex-col items-center min-h-screen overflow-hidden h-full'>
             <div className='py-4'>
                 <img className='h-[30px]' src='/egold_logo_new.png'/>
             </div>
-            {
-            success_failure ? 
                 <div className='flex flex-col items-center p-[30px] bg-[#25252f] b-rad rounded-[20px] my-5 w-[400px] border-[#6969ae] border-solid border-[2px]'>
                     <h3 className='text-white font-bold text-[16px] mb-5 text-center'>Email Verification Successfully</h3>
                     <img className='h-28' src='/success.png' />
                     <p className='text-[#c4d2fa] text-center mt-5'><a href='#'>Please click here to continue</a></p>
                 </div>
-            :
-                <div className='flex flex-col items-center p-[30px] bg-[#25252f] b-rad rounded-[20px] my-5 w-[400px] border-[#ff0000] border-solid border-[2px]'>
-                    <h3 className='text-white font-bold text-[16px] mb-5 text-center'>User Verification Error</h3>
-                    <img className='h-28' src='/fail.png' />
-                    <p className='text-[#c4d2fa] text-center mt-5'><a href='#'>Please Click here for more information</a></p>
-                </div>
-            }
         </div>
         </>
     );
